e95ae7c2ebe3d5e04814bac709b6b7c309d78f75
Julius-c <aicscc@outlook.com>
2019-07-29 15:04:44 +0800

    update

--- supervoxel.py
+++ supervoxel.py
@@ -2,13 +2,27 @@
 import numpy as np
 import sys
 import cv2
+import os
 from math import *
 
 class SLIC:
-	def __init__(self, image, supervoxels, nc):
-		rgb2lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-		self.rgb2lab = np.array([rgb2lab, rgb2lab, rgb2lab, rgb2lab, rgb2lab])
-		self.image = np.array([image, image, image, image, image])
+	def __init__(self, imagedir, supervoxels, nc):
+		self.image = np.zeros(shape=(99, 256, 256, 3))
+		self.rgb2lab = np.zeros(shape=(99, 256, 256, 3))
+		pathdir = os.listdir(imagedir)
+		count = 0
+		for s in xrange(99):
+			if s < 9:
+				alldir = "cthead-8bit00" + str(s + 1) + ".tif"
+			else:
+				alldir = "cthead-8bit0" + str(s + 1) + ".tif"
+			child = os.path.join('%s/%s' % (imagedir, alldir))
+			if os.path.isfile(child):
+				piece = cv2.imread(child)
+				labpiece = cv2.cvtColor(piece, cv2.COLOR_BGR2LAB)
+				self.image[count] = piece
+				self.rgb2lab[count] = labpiece
+				count += 1
 		self.depth = self.rgb2lab.shape[0]
 		self.height = self.rgb2lab.shape[1]
 		self.width = self.rgb2lab.shape[2]
@@ -33,7 +47,10 @@ class SLIC:
 					l1 = self.rgb2lab[z, y, x][0]
 					l2 = self.rgb2lab[z, y + 1, x][0]
 					l3 = self.rgb2lab[z, y, x + 1][0]
-					curGrad = (l1 - l2 if l1 > l2 else l2 - l1) + (l1 - l3 if l1 > l3 else l3 - l1)
+					l4 = self.rgb2lab[z + 1, y, x][0]
+					curGrad = (l1 - l2 if l1 > l2 else l2 - l1) \
+							+ (l1 - l3 if l1 > l3 else l3 - l1) \
+							+ (l1 - l4 if l1 > l4 else l4 - l1)
 					if (curGrad < lowGrad):
 						lowGrad = curGrad
 						localMin = [z, y, x]
@@ -51,28 +68,30 @@ class SLIC:
 
 	def initialize(self):
 		centers = []
-		nzs = self.depth / self.step
-		if nzs <= 0:
-			nzs = 1
-		zs = self.depth / nzs
-		if zs >= self.depth:
-			zs = 0
-		zoff = self.step / 2 if self.step < self.depth else self.depth / 2
-		for zi in xrange(nzs):
-			z = zoff + zi * zs
-			if z >= self.depth:
-				z = self.depth - zoff
+		# nzs = self.depth / self.step
+		# if nzs <= 0:
+		# 	nzs = 1
+		# zs = self.depth / nzs
+		# if zs >= self.depth:
+		# 	zs = 0
+		# zoff = self.step / 2 if self.step < self.depth else self.depth / 2
+		# for zi in xrange(nzs):
+		# 	z = zoff + zi * zs
+		# 	if z >= self.depth:
+		# 		z = self.depth - zoff
+		for z in xrange(self.step / 2, self.depth - self.step / 2, self.step):
 			for y in xrange(self.step / 2, self.height - self.step / 2, self.step):
 				for x in xrange(self.step / 2, self.width - self.step / 2, self.step):
 					oc = [z, y, x]
 					nc = self.findLowestGrad(oc)
-					ncolor = self.rgb2lab[nc[0], nc[1], nc[2]]
+					ncolor = self.rgb2lab[nc[0], nc[1], nc[2]].astype(np.int32)
 					centers.append([ncolor[0], ncolor[1], ncolor[2], nc[0], nc[1], nc[2]])
 		self.centers = np.array(centers)
 
 	def generateSuperVoxels(self):
 		for i in xrange(self.iterations):
 			self.distances = self.MAX * np.ones(self.image.shape[:3])
+			print(len(self.centers))
 			for j in xrange(len(self.centers)):
 				for z in xrange(self.centers[j][3] - self.step, self.centers[j][3] + self.step):
 					for y in xrange(self.centers[j][4] - self.step, self.centers[j][4] + self.step):
@@ -85,16 +104,17 @@ class SLIC:
 								if (dist < self.distances[z, y, x]):
 									self.distances[z, y, x] = dist
 									self.labels[z, y, x] = j
+			print("4")
 			# update center
 			for j in xrange(len(self.centers)):
 				jcluster = self.labels == j
-				self.centers[j][0:3] = np.sum(self.rgb2lab[jcluster], axis=0)
+				self.centers[j][0:3] = np.sum(self.rgb2lab[jcluster], axis = 0)
 				self.centers[j][3:] = [0, 0, 0]
 			for z in xrange(self.depth):
 				for y in xrange(self.height):
 					for x in xrange(self.width):
-						label = int(self.labels[z, y, x])
-						self.centers[label][3:] += [z, y, x]
+						j = int(self.labels[z, y, x])
+						self.centers[j][3:] += [z, y, x]
 			for j in xrange(len(self.centers)):
 				jcluster = self.labels == j
 				self.centers[j] /= np.sum(jcluster)
@@ -155,37 +175,41 @@ class SLIC:
 
 		for k in xrange(self.depth):
 		    for j in xrange(self.height):
-					for i in xrange(self.width):
-						count = 0
-						for dx, dy, dz in zip(dx26, dy26, dz26):
-							x = i + dx
-							y = j + dy
-							z = k + dz
-							if x >= 0 and x < self.width \
-								and y >= 0 and y < self.height \
-								and z >= 0 and z < self.depth:
-								if isTaken[z, y, x] == False and self.labels[k, j, i] != self.labels[z, y, x]:
-									count += 1
+				for i in xrange(self.width):
+					count = 0
+					for dx, dy, dz in zip(dx26, dy26, dz26):
+						x = i + dx
+						y = j + dy
+						z = k + dz
+						if x >= 0 and x < self.width \
+							and y >= 0 and y < self.height \
+							and z >= 0 and z < self.depth:
+							if isTaken[z, y, x] == False and self.labels[k, j, i] != self.labels[z, y, x]:
+								count += 1
 
-						if count >= 8:
-							isTaken[k, j, i] = True
-							contours.append([k, j, i])
+					if count >= 8:
+						isTaken[k, j, i] = True
+						contours.append([k, j, i])
 
 		for i in xrange(len(contours)):
 		    self.image[contours[i][0], contours[i][1], contours[i][2]] = color
 
 	def run(self):
 		self.initialize()
+		print("1")
 		self.generateSuperVoxels()
+		print("2")
 		self.enforceLabelConnectivity()
+		print("3")
 		self.displayContour((0, 0, 255))
+		print("4")
 		for i in xrange(self.depth):
 			s = "slic" + str(i) + ".jpg"
 			cv2.imwrite(s, self.image[i])
 
 
-image = cv2.imread(sys.argv[1])
+imagedir = sys.argv[1]
 supervoxels = int(sys.argv[2])
 nc = int(sys.argv[3])
-slic = SLIC(image, supervoxels, nc)
+slic = SLIC(imagedir, supervoxels, nc)
 slic.run()
